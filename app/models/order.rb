45f1794ff0d0bb3fcfd8f11a8d844d12f0b118a3
jiaowagn <wangjiaohere@gmail.com>
2017-10-01 13:37:56 +0800

    add product_list model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   validates_presence_of :billing_name, :billing_address, :shipping_name, :shipping_address
 
-  belongs_to :user 
+  belongs_to :user
+  has_many :product_lists 
 end
